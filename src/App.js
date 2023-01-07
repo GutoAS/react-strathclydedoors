3cdf20236c5b1312ce1d9bb1e898e1d59881b59d
GutoAS <agostinhosoberano2@gmail.com>
2023-01-07 12:30:13 +0200

    removing unnecessary jsx

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-import CreateDoor from "./Screen/CreateDoor";
 import Home from "./Screen/Home";
-// import LandingPage from "./Screen/LandingPage";
 import NewLanding from "./Screen/NewLanding";
-import PreDefine from "./Screen/PreDefine";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CustomDoor from "./Screen/CustomDoor";
 import Contact from "./Screen/Contact";
-import Header from './Component/Header/Header';
+import Header from "./Component/Header/Header";
 import About from "./Screen/About";
-import React from 'react'
+import React from "react";
 import Footer from "./Component/footer/Footer";
 
 function App() {
   return (
     <>
-    <Header />
-    
+      <Header />
+
       <BrowserRouter>
         <Routes>
           <Route element={<NewLanding />} path="/" />
           <Route element={<Home />} path="/door" />
-          <Route element={<PreDefine />} path="/door-type" />
-          <Route element={<CreateDoor />} path="/create-door" />
-          <Route element={<CustomDoor />} path="/custom-door" />
           <Route element={<Contact />} path="/contact" />
           <Route element={<About />} path="/about" />
-
         </Routes>
       </BrowserRouter>
-     <Footer/>
+      <Footer />
     </>
   );
 }
--- src/Screen/NewLanding.jsx
+++ src/Screen/NewLanding.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import About from '../Component/About/About'
-import Benefits from '../Component/Benefits/Benefits'
-import Calltoaction from '../Component/CalltoAction/Calltoaction'
-import Diff from '../Component/Diff/Diff'
-import Hero from '../Component/Hero/Hero'
-import Projects from '../Component/Projects/Projects'
-import Quality from '../Component/Qualitites/Quality'
-import Services from '../Component/Services/Services'
-// import Testi from '../Component/Testimonial/testi/Testi'
-import TestiBox from '../Component/Testimonial/TestiBox'
-
+import React from "react";
+import About from "../Component/About/About";
+import Benefits from "../Component/Benefits/Benefits";
+import Calltoaction from "../Component/CalltoAction/Calltoaction";
+import Diff from "../Component/Diff/Diff";
+import Hero from "../Component/Hero/Hero";
+import Projects from "../Component/Projects/Projects";
+import Quality from "../Component/Qualitites/Quality";
+import Services from "../Component/Services/Services";
+import TestiBox from "../Component/Testimonial/TestiBox";
 
 const NewLanding = () => {
   return (
-   <>
-   <Hero/>
-   <Services/>
-   <Benefits/>
-   <Projects/>
-   <Quality/>
-   <Diff/>
-   <TestiBox/>
-   <About/>
-   <Calltoaction/>
-   </>
-  )
-}
+    <>
+      <Hero />
+      <Services />
+      <Benefits />
+      <Projects />
+      <Quality />
+      <Diff />
+      <TestiBox />
+      <About />
+      <Calltoaction />
+    </>
+  );
+};
 
-export default NewLanding
+export default NewLanding;
